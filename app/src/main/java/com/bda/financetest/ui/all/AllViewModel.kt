0096828ee373844122a8a26a221bc9d387adc688
denbelobaba <denbelobaba@gmail.com>
2021-02-27 10:45:30 +0300

    create project

---
+++ app/src/main/java/com/bda/financetest/ui/all/AllViewModel.kt
@@ -0,0 +1,13 @@
+package com.bda.financetest.ui.all
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AllViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is all Fragment"
+    }
+    val text: LiveData<String> = _text
+}
